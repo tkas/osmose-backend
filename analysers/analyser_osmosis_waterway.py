194e34a0ce393d9930b1ca4dc7521beb8951b28a
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2026-05-22 17:54:24 +0200

    Add waterway=fish_pass in analyser_osmosis_waterway #2747

--- analysers/analyser_osmosis_waterway.py
+++ analysers/analyser_osmosis_waterway.py
@@ -85,7 +85,7 @@ FROM
         ways.id = way_nodes.way_id AND
         ways.tags != ''::hstore AND
         ways.tags?'waterway' AND
-        ways.tags->'waterway' IN ('stream', 'river', 'canal', 'drain', 'ditch', 'tidal_channel', 'flowline')
+        ways.tags->'waterway' IN ('stream', 'river', 'canal', 'drain', 'ditch', 'tidal_channel', 'flowline', 'fish_pass')
 """
 
 sql23 = """
