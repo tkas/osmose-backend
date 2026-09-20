1d2fe24998a57a5c59d35f6eb876d46192f5c129
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2026-09-20 15:34:08 +0200

    Fix mypy config

--- modules/downloader.py
+++ modules/downloader.py
@@ -24,7 +24,7 @@ import os
 import time
 import requests
 from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry
+from urllib3.util.retry import Retry
 from datetime import datetime
 from typing import Dict, Optional
 from . import config
